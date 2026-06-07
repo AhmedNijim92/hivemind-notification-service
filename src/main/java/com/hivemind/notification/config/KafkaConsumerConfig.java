5bc05b66ebc98d508e863eb56dfef8e9575201a1
AhmedNijim92 <ahmed.nijim@outlook.com>
2026-06-07 19:03:27 +0200

    feat: update with deployment fixes and registry images

--- src/main/java/com/hivemind/notification/config/KafkaConsumerConfig.java
+++ src/main/java/com/hivemind/notification/config/KafkaConsumerConfig.java
@@ -1,5 +1,10 @@
 package com.hivemind.notification.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.hivemind.common.event.MeetingStartedEvent;
+import com.hivemind.common.event.PostCreatedEvent;
+import com.hivemind.common.event.UserCreatedEvent;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.beans.factory.annotation.Value;
@@ -8,7 +13,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
-import org.springframework.kafka.support.serializer.JsonDeserializer;
+import org.springframework.kafka.support.converter.RecordMessageConverter;
+import org.springframework.kafka.support.converter.StringJsonMessageConverter;
+import org.springframework.kafka.support.mapping.DefaultJackson2JavaTypeMapper;
+import org.springframework.kafka.support.mapping.Jackson2JavaTypeMapper;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -22,19 +30,40 @@ public class KafkaConsumerConfig
     @Bean
     public ConsumerFactory<String, Object> consumerFactory()
     {
-        JsonDeserializer<Object> deserializer = new JsonDeserializer<>(Object.class);
-        deserializer.setRemoveTypeHeaders(false);
-        deserializer.addTrustedPackages("*");
-        deserializer.setUseTypeMapperForKey(true);
-
         Map<String, Object> config = new HashMap<>();
         config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         config.put(ConsumerConfig.GROUP_ID_CONFIG, "notification-service");
         config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, deserializer);
+        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         config.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
 
-        return new DefaultKafkaConsumerFactory<>(config, new StringDeserializer(), deserializer);
+        return new DefaultKafkaConsumerFactory<>(config);
+    }
+
+    @Bean
+    public RecordMessageConverter messageConverter()
+    {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        // The producers serialize LocalDateTime as arrays (no JavaTimeModule on their side).
+        // JavaTimeModule can read both array format [2026,6,6,21,17,31,746947697] and string format.
+        // We do NOT disable WRITE_DATES_AS_TIMESTAMPS because we need to READ array format.
+
+        StringJsonMessageConverter converter = new StringJsonMessageConverter(objectMapper);
+
+        DefaultJackson2JavaTypeMapper typeMapper = new DefaultJackson2JavaTypeMapper();
+        typeMapper.setTypePrecedence(Jackson2JavaTypeMapper.TypePrecedence.TYPE_ID);
+        typeMapper.addTrustedPackages("com.hivemind.common.event");
+
+        // Map the type IDs sent by producers to our local event classes
+        Map<String, Class<?>> mappings = new HashMap<>();
+        mappings.put("com.hivemind.common.event.UserCreatedEvent", UserCreatedEvent.class);
+        mappings.put("com.hivemind.common.event.PostCreatedEvent", PostCreatedEvent.class);
+        mappings.put("com.hivemind.common.event.MeetingStartedEvent", MeetingStartedEvent.class);
+        typeMapper.setIdClassMapping(mappings);
+
+        converter.setTypeMapper(typeMapper);
+        return converter;
     }
 
     @Bean
@@ -43,6 +72,7 @@ public class KafkaConsumerConfig
         ConcurrentKafkaListenerContainerFactory<String, Object> factory =
                 new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(consumerFactory());
+        factory.setRecordMessageConverter(messageConverter());
         return factory;
     }
 }
